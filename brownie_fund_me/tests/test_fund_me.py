4474df17079cdff62d1b6cfd746027c943b8a3a8
yawsaii <bernardsaii20@gmail.com>
2022-03-21 04:46:55 +0000

    brownie-fund-me contract

---
+++ brownie_fund_me/tests/test_fund_me.py
@@ -0,0 +1,33 @@
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account
+from scripts.deploy import deploy_fund_me
+from brownie import network, accounts, exceptions
+import pytest
+
+""" The tests below were implemented using the `mock` method"""
+
+
+def test_can_fund_and_withdraw():
+    account = get_account()
+    fund_me = deploy_fund_me()
+    entrance_fee = fund_me.getEntranceFee() + 100
+    tx = fund_me.fund({"from": account, "value": entrance_fee})
+    tx.wait(1)
+    # Asserting
+    assert fund_me.addressToAmountFunded(account.address) == entrance_fee
+
+    # Testing withdraw
+    tx2 = fund_me.withdraw({"from": account})
+    tx2.wait(1)
+    # Asserting
+    assert fund_me.addressToAmountFunded(account.address) == 0
+
+
+def test_only_owner_can_withdraw():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip("only for local testing")
+
+    fund_me = deploy_fund_me()
+    bad_actor = accounts.add()
+    fund_me.withdraw({"from": bad_actor})
+    with pytest.raises(exceptions.VirtualMachineError):
+        fund_me.withdraw({"from": bad_actor})
